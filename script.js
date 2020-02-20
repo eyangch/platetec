1a04017660b775dcf132e43a2d21ebe28e8fedc8
eyangch <eyangch@gmail.com>
2020-02-19 18:32:01 -0600

    Add GUI and images

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function setup(){
+const w = 1280, h = 720;
+var left = ["continental", "oceanic"];
+var right = ["continental", "oceanic"];
+var gui;
 
+function setup(){
+    createCanvas(1280, 720);
+    gui = createGui();
+    gui.addGlobals("left", "right");
 }
 
 function draw(){
-
+    background(255);
+    text(left, 50, 50);
+    text(right, 50, 100);
+    rect(500, 500, 50, 50);
 }
