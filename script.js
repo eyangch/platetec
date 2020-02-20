4de5941dcfb1682c04c75405ef770f4e7f4d2211
eyangch <eyangch@gmail.com>
2020-02-19 19:58:57 -0600

    Add some stuff

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
 const w = 1280, h = 720;
-var left = ["continental", "oceanic"];
-var right = ["continental", "oceanic"];
+var tarr = ["continental/continental", "continental/oceanic", "oceanic/oceanic"];
+var type = ["continental/continental", "continental/oceanic", "oceanic/oceanic"];
 var gui;
+var cc, co, oo;
 
 function setup(){
-    createCanvas(1280, 720);
+    createCanvas(640, 480);
     gui = createGui();
-    gui.addGlobals("left", "right");
+    gui.addGlobals("type");
+    cc = loadImage("https://eyangch.github.io/platetec/img/cc.png");
+    co = loadImage("https://eyangch.github.io/platetec/img/co.png");
+    oo = loadImage("https://eyangch.github.io/platetec/img/oo.png");
+    ccc = loadImage("https://eyangch.github.io/platetec/img/ccc.png");
+    cco = loadImage("https://eyangch.github.io/platetec/img/cco.png");
+    dcc = loadImage("https://eyangch.github.io/platetec/img/dcc.png");
+    doo = loadImage("https://eyangch.github.io/platetec/img/doo.png");
+    tccup = loadImage("https://eyangch.github.io/platetec/img/tccup.png");
+    tccdown = loadImage("https://eyangch.github.io/platetec/img/tccdown.png");
 }
 
 function draw(){
+    var conv = false, divr = false, utra = false, dtra = false;
+    var x = 0, y = 0;
     background(255);
-    text(left, 50, 50);
-    text(right, 50, 100);
-    rect(500, 500, 50, 50);
+    text(type, 50, 50);
+    if(keyIsDown(RIGHT_ARROW)){
+        conv = true;
+    }else if(keyIsDown(LEFT_ARROW)){
+        divr = true;
+    }else if(keyIsDown(UP_ARROW)){
+        utra = true;
+    }else if(keyIsDown(DOWN_ARROW)){
+        dtra = true;
+    }
+    if(conv){
+        y = random() * 5;
+    }
+    if(utra || dtra){
+        y = random() * 20;
+    }
+    if(type == tarr[0]){
+        if(conv){
+            image(ccc, x, y);
+        }else if(divr){
+            image(dcc, x, y);
+        }else if(utra){
+            image(tccup, x, y);
+        }else if(dtra){
+            image(tccdown, x, y);        
+        }else{
+            image(cc, 0, 0);
+        }
+    }else if(type == tarr[1]){
+        if(conv){
+            image(cco, x, y);
+        }else{
+            image(co, 0, 0);
+        }
+    }else{
+        if(divr){
+            image(doo, x, y);
+        }else{
+            image(oo, 0, 0);
+        }
+    }
 }
